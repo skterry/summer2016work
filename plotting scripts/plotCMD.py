b16e9a7b0f36d9b1dec3bb3f67ec324450276b97
Ishaan Gandhi <ishaangandhi@gmail.com>
2016-07-01 14:54:40 -0400

    plot cmd fix

--- plotting scripts/plotCMD.py
+++ plotting scripts/plotCMD.py
@@ -5,33 +5,33 @@ x = []
 y = []
 s = 0.25
 
-bulgeFile = open('f2catalog2.csv', 'r')
+#bulgeFile = open('f2catalog2.csv', 'r')
 #diskFile = open('/Users/SeanTerry/Desktop/finalcmd/disk/diskmatlaboutput/plotpointsJ-Hdisk.csv', 'r')
 
-sepFile = bulgeFile.read().split('\r')
+sepFile = open('F555_814.out')#bulgeFile.read().split('\r')
 ## sepDisk = diskFile.read().split('\r')
 
-bulgeFile.close()
+#bulgeFile.close()
 
 
 for plotPair in sepFile:
-	xandy = plotPair.split(',')
-	x.append(float(xandy[3]))
+	xandy = plotPair.split(' ')
+	x.append(float(xandy[7]))
 	y.append(float(xandy[5]))
 	
 # for plotPair in sepDisk:
-	aandb = plotPair.split(',')
-	x.append(float(xandy[3]))
+	aandb = plotPair.split(' ')
+	x.append(float(xandy[7]))
 	y.append(float(xandy[5]))
 	
 
 
-plt.scatter(x, y, s)
+plt.scatter(x, y, s=s, alpha = 0)
 
 plt.title('CMD Stanek Field')
-plt.xlabel('F110W - F160W')
+plt.xlabel('F555W - F814W')
 plt.ylabel('F160W Instrumental Mag')
 plt.gca().invert_yaxis()
-pylab.xlim([-0.5,3.0])
+pylab.xlim([-0.5,5.0])
 
 plt.show()
