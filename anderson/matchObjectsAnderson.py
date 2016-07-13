0475d2af748d21f176f40b788a898ca5ec9606f2
Ishaan Gandhi <ishaangandhi@gmail.com>
2016-07-13 15:13:43 -0400

    anderson

--- anderson/matchObjectsAnderson.py
+++ anderson/matchObjectsAnderson.py
@@ -1,56 +1,101 @@
 import sys
 from decimal import Decimal
+from time import gmtime, strftime
 
-OUTPUTNAME = 'matchedStars555-814.out'
+OUTPUTNAME = 'matchedStars555-814.'
+OUTPUTNAME += strftime("%Y-%m-%d.%H-%M-%S", gmtime())
+OUTPUTNAME += '.out'
 
-file1 = open('ib3m814.out')#open('ibom13suq_flc.chip1.out')
-file2 = open('ib3m555.out')#open('ib3m13lcq_flc.chip1.out')
+ib3m814 = open('ib3m814.out')
+ib3m555 = open('ib3m555.out')
+ibom814 = open('ibom814.out')
+
+ib3m814List = []
+ib3m555List = []
+ibom814List = []
 
 TOLERANCE = .1
-
-file1StarsList = []
-file2StarsList = []
-
-if len(sys.argv) == 3:
-    file1 = open(sys.argv[1])
-    file2 = open(sys.argv[2])
-
-print("----------------------------------------")
-print("Running Script: Match Objects")
+MATCHBYPOS = 0
+MATCHBYMAG = 1
+RANGE = .5
 
 def lineToStar(input):
     returningStar = input.split()
     for i in range(len(returningStar)):
         returningStar[i] = float(returningStar[i])
-   # returningStar[3]=10**(returningStar[3]/-2.5)
     return returningStar
-    
-curIndex = 0
-for line in file1:
-    curStar = lineToStar(line)
-    curStar.append(curIndex)
-    file1StarsList.append(curStar)
-    curIndex+=1
-    #FEATURES ARE 0-qual, 1-xpos, 2-ypos, 3-mag
-        
-curIndex = 0
-for line in file2:
-    curStar = lineToStar(line)
-    curStar.append(curIndex)
-    file2StarsList.append(curStar)
-    curIndex+=1
 
-totalStars = len(file1StarsList)        
-for star1 in file1StarsList:
-    if star1[4] %100 == 0:
-        print("processing star {0} of {1}".format(star1[4], totalStars))
-    for star2 in file2StarsList:
-        if star1[3]*(1-TOLERANCE) < star2[3] < star1[3]*(1+TOLERANCE):
-            if star1[1]-2<star2[1]<star1[1]+2:
-                if -2<star1[2]-star2[2]<2:
-                    dx = round(star1[1]-star2[1], 4)
-                    dy = round(star1[2]-star2[2], 4)
-                    with open (OUTPUTNAME, 'a') as f: f.write ("{0} {1} {2} {3} {4} {5} {6} {7}\n".format(star1[4], star2[4], dx, dy, star1[3], star2[3]-star1[3], star1[1], star1[2])) 
-                    file2StarsList.remove(star2)
-                    break
+def loadFile(filename):
+    curIndex = 0
+    returnableList = []
+    for line in filename:
+        curStar = lineToStar(line)
+        curStar.append(curIndex)
+        returnableList.append(curStar)
+        curIndex+=1
+    return returnableList
+    #FEATURES ARE 0-qual, 1-xpos, 2-ypos, 3-mag, 4-index
+
+def matchStarByQual(star1, matchingList):
+    star1Qual = star1[0]
+    star1XPos = star1[1]
+    star1YPos = star1[2]
+    star1Mag = star1[3]
+    
+    for star2 in matchingList:
+        #check that qual is within tolerance:
+        #if star1Qual*(1-TOLERANCE) < star2[0] < star1Qual*(1+TOLERANCE): 
+        if -RANGE < star2[1]-star1XPos < RANGE: #check that x pos is within +/- .5
+            if -RANGE < star2[2]-star1YPos < RANGE: #check that y pos is within +/- .5
+                matchingList.remove(star2)
+                color = star2[3] - star1Mag #calculate 555 mag - 814 mag
+                return color
+    return None
                     
+def matchStarByMag(star1, matchingList):
+    star1Qual = star1[0]
+    star1XPos = star1[1]
+    star1YPos = star1[2]
+    star1Mag = star1[3]
+    
+    for star2 in matchingList:
+        #check that magnitude is within tolerance:
+        if star1Mag*(1-TOLERANCE) > star2[3] > star1Mag*(1+TOLERANCE): 
+            if -5 < star1XPos-star2[1] < 1: #check that x pos is within jitter
+                if .5 < star1YPos-star2[2] < 2.5: #check that y pos is within jitter
+                    matchingList.remove(star2)
+                    dx = star1XPos-star2[1] #calculate dx
+                    dy = star1YPos-star2[2] #calculate dy
+                    return (dx,dy)
+    return (None, None)
+    
+def main():
+    ib3m814List = loadFile(ib3m814)
+    ib3m555List = loadFile(ib3m555)
+    ibom814List = loadFile(ibom814)
+    totalStars = len(ib3m814List)
+    
+    for ib3m814Star in ib3m814List:
+        curIndex = ib3m814Star.pop(4)
+        if curIndex %100 == 0:
+            print("processing star {0} of {1}".format(curIndex, totalStars))
+        
+        #1. match objects between 814 and 555 in 2010
+        curColor = matchStarByQual(ib3m814Star, ib3m555List)
+        if curColor is None:
+            continue
+        
+        #2. match objects between 2010 and 2012 in 814
+        (curDx, curDy) = matchStarByMag(ib3m814Star, ibom814List)
+        if curDx is None:
+            continue
+        
+        ib3m814Star.extend([curColor, curDx, curDy])
+        outputStr = " ".join(map(str, ib3m814Star))
+        with open (OUTPUTNAME, 'a') as f: f.write (outputStr+"\n")
+
+print("----------------------------------------")
+print("Running Script: Match Objects")
+
+if __name__ == "__main__":
+    main()
