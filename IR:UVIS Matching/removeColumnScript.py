84f6e40e187a287183503aaf005ac84400781807
Ishaan Gandhi <ishaangandhi@gmail.com>
2016-07-25 15:36:59 -0400

    .

--- IR:UVIS Matching/removeColumnScript.py
+++ IR:UVIS Matching/removeColumnScript.py
@@ -10,7 +10,7 @@ for line in input:
     args = line.split()
     
     if args[10] == "1" and args[15] != "99.999":
-        outputString+="{0} {1} {2} {3} {4} {5} {6} {7}\n".format(args[2], args[3], args[10], args[15], args[23])
+        outputString+="{0} {1} {2} {3} {4}\n".format(args[2], args[3], args[10], args[15], args[23])
 	
         
 with open (OUTPUTNAME, 'a') as f: f.write (outputString) 
