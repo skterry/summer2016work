0e6ee94fbd53beeb72f3d42b90893952cff0c142
Ishaan Gandhi <ishaangandhi@gmail.com>
2016-07-25 15:36:20 -0400

    rm col scr

---
+++ IR:UVIS Matching/removeColumnScript.py
@@ -0,0 +1,16 @@
+import sys
+
+OUTPUTNAME = 'F814.out'
+INPUTNAME = "F814WChip1Output"
+#F160Woutput
+input = open(INPUTNAME)
+outputString = ""
+
+for line in input:
+    args = line.split()
+    
+    if args[10] == "1" and args[15] != "99.999":
+        outputString+="{0} {1} {2} {3} {4} {5} {6} {7}\n".format(args[2], args[3], args[10], args[15], args[23])
+	
+        
+with open (OUTPUTNAME, 'a') as f: f.write (outputString) 
